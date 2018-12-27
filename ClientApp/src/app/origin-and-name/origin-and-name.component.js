285c104223034560e5381bfa4b897c0e3cb1c7ed
mccullen <mccullen_j@lynchburg.edu>
2018-12-27 13:52:48 -0500

    Add origin and name component

---
+++ ClientApp/src/app/origin-and-name/origin-and-name.component.js
@@ -0,0 +1,11 @@
+/*
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+}
+*/
+//# sourceMappingURL=origin-and-name.component.js.map
---
+++ ClientApp/src/app/origin-and-name/origin-and-name.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'origin-and-name',
+  templateUrl: './origin-and-name.component.html'
+})
+export class OriginAndNameComponent {
+}
+/*
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+}
+*/
