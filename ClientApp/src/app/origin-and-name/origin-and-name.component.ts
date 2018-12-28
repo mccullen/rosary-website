4927546e49d101729cb58a0f1b31ad110b5dd49b
mccullen <mccullen_j@lynchburg.edu>
2018-12-27 20:42:07 -0500

    Add 'what is the rosary' and 'what prayers does the rosary consist of' to origin and name page

--- ClientApp/src/app/origin-and-name/origin-and-name.component.ts
+++ ClientApp/src/app/origin-and-name/origin-and-name.component.ts
@@ -6,13 +6,3 @@ import { Component } from '@angular/core';
 })
 export class OriginAndNameComponent {
 }
-/*
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-})
-export class HomeComponent {
-}
-*/
