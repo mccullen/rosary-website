11673d4008139107f48db15a9d5e74b7815216b1
mccullen <mccullen_j@lynchburg.edu>
2018-12-30 17:44:48 -0500

    Add link to todays mysteries on the home page

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -5,4 +5,44 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  mysterySeries: string;
+  mysteryLink: string;
+  day: number;
+
+  constructor() {
+    this.day = new Date().getDay();
+    if (this.joyfulMysteriesDay()) {
+      this.mysterySeries = "Joyful Mysteries";
+      this.mysteryLink = "http://www.vatican.va/special/rosary/documents/misteri_gaudiosi_en.html";
+    } else if (this.luminousMysteriesDay()) {
+      this.mysterySeries = "Mysteries of Light";
+      this.mysteryLink = "http://www.vatican.va/special/rosary/documents/misteri_luminosi_en.html";
+    } else if (this.sorrowfulMysteriesDay()) {
+      this.mysterySeries = "Sorrowful Mysteries";
+      this.mysteryLink = "http://www.vatican.va/special/rosary/documents/misteri_dolorosi_en.html";
+    } else if (this.gloriousMysteriesDay()) {
+      this.mysterySeries = "Glorious Mysteries";
+      this.mysteryLink = "http://www.vatican.va/special/rosary/documents/misteri_gloriosi_en.html";
+    }
+  }
+
+  joyfulMysteriesDay() {
+    // Mondays and Saturdays
+    return this.day === 1 || this.day === 6;
+  }
+
+  luminousMysteriesDay() {
+    // Thursdays
+    return this.day === 4;
+  }
+
+  sorrowfulMysteriesDay() {
+    // Tuesdays and Fridays
+    return this.day === 2 || this.day === 5;
+  }
+
+  gloriousMysteriesDay() {
+    // Wednesdays and Sundays
+    return this.day === 3 || this.day === 0;
+  }
 }
