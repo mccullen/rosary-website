7a075dff137b32b686b908b1a03535cafbc81fa3
mccullen <mccullen_j@lynchburg.edu>
2018-12-30 22:58:38 -0500

    Add footer quote

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { App } from './app';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private app: App) { }
 }
