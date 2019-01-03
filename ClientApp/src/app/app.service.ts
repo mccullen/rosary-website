1839da8085dd07e5d63fc2846e5a3bdf50a2bb9a
mccullen <mccullen_j@lynchburg.edu>
2019-01-03 08:48:12 -0500

    Fix merge issues.

--- ClientApp/src/app/app.service.ts
+++ ClientApp/src/app/app.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 
 @Injectable()
 export class AppService {
+  baseUrl: string;
   secretOfTheRosaryLink: string =
     "http://www.montfortian.info/writings/the-admirable-secret-of-the-rosary.html";
+
+  constructor(@Inject('BASE_URL') baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
 }
