d07732c3537dfccc11fb52e8adccff16c0ea64cb
mccullen <mccullen_j@lynchburg.edu>
2019-01-16 09:30:45 -0500

    Add scroll to top button

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
 
 @Component({
@@ -6,9 +6,35 @@ import { AppService } from './app.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  app: AppService;
-  constructor(app: AppService) {
-    this.app = app;
+export class AppComponent implements OnInit {
+  topBtn: any = {
+    id: "scroll-btn",
+    text: "Top",
+    scrollTopPx: 20
+  };
+
+  constructor(public app: AppService) {
+  }
+
+  ngOnInit() {
+    this.addOnScrollListener();
+  }
+
+  private addOnScrollListener() {
+    let that = this;
+    window.onscroll = function () {
+      let topBtn = that.topBtn;
+      if (document.body.scrollTop > topBtn.scrollTopPx ||
+          document.documentElement.scrollTop > topBtn.scrollTopPx) {
+        document.getElementById(topBtn.id).style.display = "block";
+      } else {
+        document.getElementById(topBtn.id).style.display = "none";
+      }
+    };
+  }
+
+  onTopBtnClick(event: any) {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   }
 }
