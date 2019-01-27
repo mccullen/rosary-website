76d0f1ba16cc8757f34f057a8699af7a7a0bd9b7
mccullen <mccullen_j@lynchburg.edu>
2019-01-27 14:57:48 -0500

    Add some dedications for a white rose for priests. Fix styles so ol tags have numbers again

--- ClientApp/src/app/dedication/dedication.component.ts
+++ ClientApp/src/app/dedication/dedication.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ContentItem, DefaultContentItem } from '../resources/content-item';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-dedication',
@@ -9,9 +11,33 @@ export class DedicationComponent implements OnInit {
 
   static dedicationPath: string = "dedication";
 
-  constructor() { }
+  toPriests: ContentItem = new DefaultContentItem(
+    "to-priests",
+    "A White Rose for Priests",
+    DedicationComponent.dedicationPath);
+
+  whyWasTheRosaryGiven: ContentItem = new DefaultContentItem(
+    "why-was-the-rosary-given",
+    "Why has Almighty God given priests the Rosary?",
+    DedicationComponent.dedicationPath);
+
+  howPriestsSpreadDevotion: ContentItem = new DefaultContentItem(
+    "how-priests-spread-devotion",
+    "How must priests spread devotion to the Rosary?",
+    DedicationComponent.dedicationPath);
+
+  howMuchGoodFromPreaching: ContentItem = new DefaultContentItem(
+    "how-much-good-from-preaching",
+    "How much good comes from priests who preach devotion to the Rosary?",
+    DedicationComponent.dedicationPath);
+
+  whyPreachingIsntEnough: ContentItem = new DefaultContentItem(
+    "why-preaching-isnt-enough",
+    "Why isn’t merely preaching the Rosary enough?",
+    DedicationComponent.dedicationPath);
+
+  constructor(public appService: AppService) { }
 
   ngOnInit() {
   }
-
 }
